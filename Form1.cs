778873ad2cd466269e18df41ebda51be35b1febd
yesbe97 <vandrielnicky@gmail.com>
2016-09-12 12:05:34 +0200

    commit 19-9
    started adding functions for opening *.txt file

--- Form1.cs
+++ Form1.cs
@@ -7,6 +7,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.IO;
+using System.Text;
+using System.Diagnostics;
 
 namespace kladblok
 {
@@ -24,7 +27,25 @@ namespace kladblok
 
         private void aboutToolStripMenuItem_Click(object sender, EventArgs e)
         {
-            MessageBox.Show("");
+            MessageBox.Show("© Nicky van Driel - MBiC14A0 - 2016");
+        }
+
+        private void openToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.Filter = "Text Files (.txt)|*.txt|All Files (*.*)|*.*";
+            var userClickedOK = openFileDialog.ShowDialog();
+            // Open the selected file to read.
+            //DialogResult fileStream = openFileDialog.FileName;
+
+            //textBox1.Text = System.IO.File.ReadAllLines(openFileDialog.FileName);
+
+            //using (System.IO.StreamReader reader = new System.IO.StreamReader(fileStream))
+            //{
+            //    // Read the first line from the file and write it the textbox.
+            //    textBox1.Text = reader.ReadAllText();
+            //}
+           // fileStream.Close();
         }
     }
 }
